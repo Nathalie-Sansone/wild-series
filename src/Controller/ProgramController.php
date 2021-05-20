135fdc1486ffd51383ed3cbef610b79c55c8a2e2
Nathalie-Sansone <nathalie.sansone@gmail.com>
2021-05-20 16:11:40 +0200

    Challenge symfony routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,13 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 
 {
 
     /**
 
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
 
      */
 
@@ -29,4 +32,21 @@ class ProgramController extends AbstractController
         ]);
 
     }
+
+    /**
+
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+
+     */
+
+    public function show(int $id): Response
+
+    {
+        return $this->render('program/show.html.twig', [
+
+            'id' => $id,
+
+        ]);
+    }
+
 }
